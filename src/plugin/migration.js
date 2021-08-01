e6b610e837646a370a93e5e89842f48082328bbd
Vikas Kumar <your_email@abc.com>
2021-08-01 21:26:03 +0530

    added staging env

--- src/plugin/migration.js
+++ src/plugin/migration.js
@@ -2,6 +2,7 @@ const DbMigrate = require('db-migrate');
 
 const runMigration = () =>
   new Promise((resolve, reject) => {
+    console.log(JSON.stringify(process.env));
     const dbMigrate = DbMigrate.getInstance(true);
     dbMigrate.silence(true);
 
